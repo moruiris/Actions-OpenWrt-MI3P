8718b2f0e0319a02a43fef6bf6c55597216ca386
moruiris <53965900+moruiris@users.noreply.github.com>
2021-06-16 13:24:49 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
-# Copyright (c) 2019-2020 P3TERX <https://github.com/hjp521/OpenWrt-mi3p>
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
-# https://github.com/hjp521/OpenWrt-mi3p
+# https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
@@ -14,4 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git moruiris https://github.com/moruiris/OpenWrt-packages' feeds.conf.default
+# sed -i '$a src-git moruiris https://github.com/moruiris/openwrt-packages' feeds.conf.default
+
+# 删除lede源码自带主题luci-theme-argon
+rm -rf ./package/lean/luci-theme-argon
